3d87404b7150cab4cfeb6f6088e66b14f4543be5
Sushant Diliprao Manjare <Sushant.Manjare@ATMECS.CORP>
2024-06-07 15:45:28 +0530

    book store testing

---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,44 @@
+package com.example.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	public List<Book> getAllBooks(){
+		return bookRepository.findAll();
+		
+	}
+	public Optional<Book> getBookById(Long id){
+		return bookRepository.findById(id);
+	}
+	
+	public Book createBook(Book book) {
+		return bookRepository.save(book);
+	}
+	
+	public Book updateBook(Long id, Book bookDetails) {
+		Book book = bookRepository.findById(id).orElseThrow(()-> new RuntimeException("Book is not Found"));
+		
+		book.setTitle(bookDetails.getTitle());
+		book.setAuthor(bookDetails.getAuthor());
+		book.setIsbn(bookDetails.getIsbn());
+		
+		return bookRepository.save(book);
+	}
+	
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
+	
+}
